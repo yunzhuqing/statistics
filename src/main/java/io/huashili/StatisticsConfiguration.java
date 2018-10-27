31776990fe92468a4e27d21dd79dc075766d37e2
yunzhuqing <june91914@gmail.com>
2018-10-28 00:11:51 +0800

    初始化项目

---
+++ src/main/java/io/huashili/StatisticsConfiguration.java
@@ -0,0 +1,9 @@
+package io.huashili;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+public class StatisticsConfiguration {
+}
---
+++ src/main/java/io/huashili/dto/LogClick.java
@@ -0,0 +1,121 @@
+package io.huashili.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 点击事件
+ */
+public class LogClick implements Serializable {
+    private static final long serialVersionUID = 8374402685453436072L;
+
+    private Long id;
+
+    private Date createTime;
+
+    private String uid;
+
+    private String ip;
+
+    private String origin;
+
+    private String topic;
+
+    private String group;
+
+    private String component;
+
+    private String value;
+
+    private String version;
+
+    private int platform;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public int getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(int platform) {
+        this.platform = platform;
+    }
+}
