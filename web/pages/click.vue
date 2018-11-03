d24006a242fd7e8b92a86a8dc510e8255e66b977
yunzhuqing <june91914@gmail.com>
2018-11-03 21:22:57 +0800

    update .gitignore

--- web/pages/click.vue
+++ web/pages/click.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="index">
         <v-chart :forceFit="true" :height="height" :data="data" :scale="scale">
             <v-tooltip />
             <v-axis />
@@ -9,52 +9,18 @@
 </template>
 
 <script>
-    const data = [
-        { year: '1', sales: 38 },
-        { year: '2', sales: 38 },
-        { year: '3', sales: 38 },
-        { year: '4', sales: 38 },
-        { year: '5', sales: 38 },
-        { year: '6', sales: 38 },
-        { year: '7', sales: 38 },
-        { year: '8', sales: 38 },
-        { year: '9', sales: 38 },
-        { year: '10', sales: 38 },
-        { year: '11', sales: 38 },
-        { year: '12', sales: 38 },
-        { year: '13', sales: 38 },
-        { year: '14', sales: 38 },
-        { year: '15', sales: 38 },
-        { year: '16', sales: 38 },
-        { year: '67', sales: 38 },
-        { year: '18', sales: 38 },
-        { year: '19', sales: 38 },
-        { year: '20', sales: 38 },
-        { year: '21', sales: 38 },
-        { year: '22', sales: 38 },
-        { year: '23', sales: 38 },
-        { year: '24', sales: 38 },
-        { year: '24', sales: 38 },
-    ];
-
-    const scale = [{
-        dataKey: 'sales',
-        tickInterval: 20,
-    }];
 
     export default {
         data() {
             return {
-                data: data,
-                scale: scale,
+                data: [],
+                scale: [],
                 height: 400,
             };
         },
         methods: {
-
         },
         mounted() {
-
         }
     };
 </script>
